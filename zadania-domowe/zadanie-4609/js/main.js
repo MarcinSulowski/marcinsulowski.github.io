98a182239c32a6f297f164c378f67dcdd5031afa
marcinsulowski <mr.sulowski@gmail.com>
2018-01-10 23:14:02 +0100

    dropdown menu for low res screens

---
+++ zadania-domowe/zadanie-4609/js/main.js
@@ -0,0 +1,21 @@
+function menuDrop() {
+ document.getElementById("droppie").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches(".dropdown-button")) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+function changeMargin() {
+    document.getElementById("main-heading").style.marginTop = "250px";
+}
