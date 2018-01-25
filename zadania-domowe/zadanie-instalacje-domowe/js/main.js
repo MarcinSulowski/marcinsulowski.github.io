d19af22c491e317968986f1100bd4a3b9b1b3bfe
marcinsulowski <mr.sulowski@gmail.com>
2018-01-25 15:23:40 +0100

    corrected layout, working tooltip buttons

--- zadania-domowe/zadanie-instalacje-domowe/js/main.js
+++ zadania-domowe/zadanie-instalacje-domowe/js/main.js
@@ -1,3 +1,23 @@
-document.getElementsByClassName("tooltip-button").addEventListener("click", function() {
-    document.getElementsByClassName("tooltip").style.visibility = "visible";
-});
+var tool = document.getElementsByClassName("tooltip-button");
+for (var i = 0; i < tool.length; i++) {
+    tool[i].addEventListener("click", showTooltip);
+}
+
+function showTooltip() {
+    var toolTipText = this.childNodes[1];
+    if (toolTipText.style.visibility === 'hidden') {
+        toolTipText.style.visibility = 'visible';
+    } else {
+        toolTipText.style.visibility = 'hidden';
+    }
+    
+    var allTooltipTexts = document.getElementsByClassName('tooltip-text');
+    window.onclick = function(e) {
+    if(!e.target.matches('.tooltip-button')) {
+        if (toolTipText.style.visibility === 'visible') {
+        toolTipText.style.visibility = 'hidden';
+    } 
+    }
+}
+}
+
