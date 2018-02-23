e7390f02004f3507172c14d950a619ae3556cdd8
marcinsulowski <mr.sulowski@gmail.com>
2018-02-23 18:36:03 +0100

    margin slide 80px

--- new-kids-layout/js/main.js
+++ new-kids-layout/js/main.js
@@ -2,14 +2,14 @@ document.addEventListener('DOMContentLoaded', function () {
     menuDrop();
 });
 
-function menuDrop(e) {
+function menuDrop (e) {
     document.getElementById("dropdown-button").addEventListener('click', function (e) {
         if (this.nextElementSibling.classList.contains('show')) {
             this.nextElementSibling.removeAttribute('class');
             document.getElementById('main-heading').removeAttribute('style');
         } else {
             this.nextElementSibling.setAttribute('class', 'show');
-            document.getElementById('main-heading').style.marginTop = '300px';
+            document.getElementById('main-heading').style.marginTop = '80px';
         }
         e.stopPropagation();
     });
