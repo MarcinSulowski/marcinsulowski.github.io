e49d27be23443b8f27ebd411699a842d177d744f
marcinsulowski <mr.sulowski@gmail.com>
2018-03-05 17:11:16 +0100

    strings instead of ascii

--- password-generator/js/main.js
+++ password-generator/js/main.js
@@ -1,12 +1,3 @@
-//ASCII codes
-//65-90 uppercase letters
-//97-122 lowercase letters
-//48-57 numbers
-//33-47 specialChars
-//91-96 specialChars
-//123-126 specialChars
-
-
 document.getElementById('generate').addEventListener('click', function () {
     let passLength = parseInt(document.getElementById('pass-length').value);
     generatePassword(passLength);
@@ -18,52 +9,67 @@ document.getElementById('clipboard').addEventListener('click', function () {
     document.execCommand('Copy');
 });
 
-const charCodes = {
-    'uppercase': [65, 90],
-    'lowercase': [97, 122],
-    'numbers': [48, 57],
-    'special': [33, 47]
-};
 
 function generatePassword(num) {
 
-    let password = '';
+    let charCodes = {
+        'uppercase': 'ABCDEFGHIJKLMNOPQRSTUWXYZ',
+        'lowercase': 'abcdefghijklmnopqrstuvwxyz',
+        'numbers': '0123456789',
+        'special': '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+    };
+    
     let checked = document.querySelectorAll('input[type=checkbox]:checked');
-    let charTypes = [];
 
-    for (var i of checked) {
 
-        for (let key in charCodes) {
-            if (i.value === key) {
-                charTypes.push(charCodes[key]);
+    function readCharTypes() {
+        let charTypes = [];
+
+        for (var i of checked) {
+
+            for (let key in charCodes) {
+                if (i.value === key) {
+                    charTypes.push(charCodes[key]);
+                }
             }
         }
+        return charTypes;
     }
 
-    //looping throught functions to fill 'password' string with random characters
-    while (password.length < num) {
-        password += getRandomCharOfType(charTypes);
+    let charTypes = readCharTypes();
+
+
+    function _generatePassword(length, charTypes) {
+        let password = '';
+
+        while (password.length < length) {
+            password += getRandomCharOfType(charTypes);
+        }
+
+        function getRandomCharOfType(charTypes) {
+            let randomChar = [];
+
+            charTypes.forEach(function (element) {
+                let rand = element[(Math.floor(Math.random() * element.length))];
+                randomChar.push(rand);
+            })
+            return randomChar[Math.floor(Math.random() * randomChar.length)];
+        }
+        return password;
     }
 
-    function getRandomCharOfType(charTypes) {
-        let randomChars = [];
-        
-        charTypes.forEach(function (charRange) {
-            let min = charRange[0];
-            let max = charRange[1];
-            let randomChar = String.fromCharCode(Math.floor(Math.random() * (max - min + 1)) + min);
-            randomChars.push(randomChar);
-        })
-        
-        return randomChars[Math.floor(Math.random() * randomChars.length)];
-    }
+    let password = _generatePassword(num, charTypes);
 
-    //if none of the checkboxes is checked, display a warning message
-    if (!checked[0] && !checked[1] && !checked[2] && !checked[3]) {
-        document.getElementById('warning').setAttribute('class', 'show');
-    } else {
-        document.getElementById('password').value = password;
-        //clear the warning
-        document.getElementById('warning').removeAttribute('class', 'show');
+
+    function updateView(password) {
+        if (!checked[0] && !checked[1] && !checked[2] && !checked[3]) {
+            document.getElementById('warning').setAttribute('class', 'show');
+        } else {
+            document.getElementById('password').value = password;
+            //clear the warning
+            document.getElementById('warning').removeAttribute('class', 'show');
+        }
     }
+    updateView(password);
+
 }
