1859125afc8d19686b0a3146fd6527391d079bcd
marcinsulowski <mr.sulowski@gmail.com>
2018-03-05 18:19:38 +0100

    new password function

--- password-generator/js/main.js
+++ password-generator/js/main.js
@@ -9,67 +9,62 @@ document.getElementById('clipboard').addEventListener('click', function () {
     document.execCommand('Copy');
 });
 
+const charCodes = {
+    'uppercase': 'ABCDEFGHIJKLMNOPQRSTUWXYZ',
+    'lowercase': 'abcdefghijklmnopqrstuvwxyz',
+    'numbers': '0123456789',
+    'special': '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+};
 
 function generatePassword(num) {
 
-    let charCodes = {
-        'uppercase': 'ABCDEFGHIJKLMNOPQRSTUWXYZ',
-        'lowercase': 'abcdefghijklmnopqrstuvwxyz',
-        'numbers': '0123456789',
-        'special': '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
-    };
-    
-    let checked = document.querySelectorAll('input[type=checkbox]:checked');
-
+    const checked = document.querySelectorAll('input[type=checkbox]:checked');
+    const warning = document.getElementById('warning');
 
     function readCharTypes() {
-        let charTypes = [];
+        const chosenCharTypes = [];
 
-        for (var i of checked) {
+        for (let i of checked) {
 
             for (let key in charCodes) {
                 if (i.value === key) {
-                    charTypes.push(charCodes[key]);
+                    chosenCharTypes.push(charCodes[key]);
                 }
             }
         }
-        return charTypes;
+        return chosenCharTypes;
     }
 
-    let charTypes = readCharTypes();
-
-
-    function _generatePassword(length, charTypes) {
-        let password = '';
-
-        while (password.length < length) {
-            password += getRandomCharOfType(charTypes);
-        }
-
-        function getRandomCharOfType(charTypes) {
-            let randomChar = [];
-
-            charTypes.forEach(function (element) {
-                let rand = element[(Math.floor(Math.random() * element.length))];
-                randomChar.push(rand);
-            })
-            return randomChar[Math.floor(Math.random() * randomChar.length)];
-        }
-        return password;
-    }
-
-    let password = _generatePassword(num, charTypes);
+    const charTypes = readCharTypes();
+    const password = _generatePassword(num, charTypes);
 
 
     function updateView(password) {
         if (!checked[0] && !checked[1] && !checked[2] && !checked[3]) {
-            document.getElementById('warning').setAttribute('class', 'show');
+            warning.setAttribute('class', 'show');
         } else {
             document.getElementById('password').value = password;
-            //clear the warning
-            document.getElementById('warning').removeAttribute('class', 'show');
+            warning.removeAttribute('class', 'show');
         }
     }
     updateView(password);
+}
 
+function _generatePassword(length, charTypes) {
+    let password = '';
+
+    while (password.length < length) {
+        password += getRandomCharOfType(charTypes);
+    }
+
+    function getRandomCharOfType(charTypes) {
+        const randomChar = [];
+
+        charTypes.forEach(function (element) {
+            let rand = element[(Math.floor(Math.random() * element.length))];
+            randomChar.push(rand);
+        })
+        return randomChar[Math.floor(Math.random() * randomChar.length)];
+    }
+    return password;
 }
