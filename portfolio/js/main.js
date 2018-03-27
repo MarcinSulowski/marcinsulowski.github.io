cfef2f4637e2a9a434cb14e6d0da555d00d9baaa
marcinsulowski <mr.sulowski@gmail.com>
2018-03-27 09:27:32 +0200

    works section draft

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,12 +1,16 @@
 const themeSwitch = document.getElementById('theme-switch');
 const allLinks = document.querySelectorAll('a');
+const projectItems = document.querySelectorAll('.works-item');
 
 themeSwitch.addEventListener('click', function () {
-    document.body.classList.toggle('day-theme');
+    document.body.classList.toggle('night-theme');
     
     for (let i of allLinks) {
-        i.classList.toggle('day-theme');
+        i.classList.toggle('night-theme');
     }
-
+    
+    for (let i of projectItems) {
+        i.classList.toggle('night-theme');
+    }
 
 })
