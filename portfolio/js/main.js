afd5c4f4a6b5198e337d4d68ecb0540697ca0353
marcinsulowski <mr.sulowski@gmail.com>
2018-03-30 13:37:32 +0200

    fixed nav

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,29 +1,30 @@
 const themeSwitch = document.getElementById('theme-switch');
 const allLinks = document.querySelectorAll('a');
 const projectItems = document.querySelectorAll('.projects-item');
+const mainNav = document.getElementById('main-nav');
 
 themeSwitch.addEventListener('click', function () {
     document.body.classList.toggle('night-theme');
-    
+
     for (let i of allLinks) {
         i.classList.toggle('night-theme');
     }
-    
+
     for (let i of projectItems) {
         i.classList.toggle('night-theme');
     }
 
+    mainNav.classList.toggle('night-theme');
+
 })
 
-//
-//const mq = window.matchMedia( "(max-width: 900px)" );
-//const itemNo = document.querySelectorAll('.item-number');
-//
-//if (mq.matches) {
-//    for (let i of itemNo) {
-//        i.innerHTML = '<sub>0</sub>' + 1;
-//    }
-//    
-//} else {
-//  // window width is less than 500px
-//}
+window.onscroll = function () {
+    let scroll = window.scrollY;
+    const navOffset = mainNav.getBoundingClientRect();
+
+    if (scroll > 230) {
+        mainNav.classList.add('fixed');
+    } else {
+        mainNav.classList.remove('fixed');
+    }
+};
