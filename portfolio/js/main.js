11eed02568c2033d882146306e1fae8d37632f6f
marcinsulowski <mr.sulowski@gmail.com>
2018-04-13 12:25:22 +0200

    about section finished

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -41,39 +41,13 @@ window.onscroll = function () {
     
     const mainNav = document.getElementById('main-nav');
     
-    if ( this.scrollY > mainNav.offsetTop ) {
+    if ( this.scrollY > 189 ) {
         mainNav.classList.add('fixed');
     } else {
         mainNav.classList.remove('fixed');
     }
-
-//    if (this.scrollY < (this.innerHeight)) {
-//        deactivateLinks();
-//        sideNavLinks[0].classList.add('active');
-//    } else if (this.scrollY > (this.innerHeight) && this.scrollY < ((this.innerHeight) * 2)) {
-//        deactivateLinks();
-//        sideNavLinks[1].classList.add('active');
-//    } else if (this.scrollY > ((this.innerHeight) * 2) && this.scrollY < ((this.innerHeight) * 3)) {
-//        deactivateLinks();
-//        sideNavLinks[2].classList.add('active');
-//    } else if (this.scrollY > ((this.innerHeight) * 3) && this.scrollY < ((this.innerHeight) * 4)) {
-//        deactivateLinks();
-//        sideNavLinks[3].classList.add('active');
-//    }
 };
-//
-//const deactivateLinks = () => {
-//    for (let i of sideNavLinks) {
-//        i.classList.remove('active');
-//    }
-//}
-//
-//sideNav.addEventListener('click', function (e) {
-//    if (e.target.matches('a')) {
-//        deactivateLinks();
-//        e.target.classList.add('active');
-//    }
-//})
+
 
 function runTyping() {
     const outputElement = document.getElementById('output');
