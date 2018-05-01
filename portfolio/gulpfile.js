818a44353d632814979b88b72c03f2b9bbebeff9
marcinsulowski <mr.sulowski@gmail.com>
2018-05-01 13:25:43 +0200

    scripts for minification

--- portfolio/gulpfile.js
+++ portfolio/gulpfile.js
@@ -1,10 +1,15 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    useref = require('gulp-useref'),
+    uglify = require('gulp-uglify'),
+    gulpIf = require('gulp-if'),
+    cssnano = require('gulp-cssnano'),
+    rename = require('gulp-rename');
 
 gulp.task('sass', function () {
+    
     return gulp.src('scss/**/*.scss')
         .pipe(sass())
-    // dodajemy obsługę błedow, watch się nie zatrzyma
         .on('error', function (err) {
             console.log(err.toString());
             this.emit('end');
@@ -14,4 +19,20 @@ gulp.task('sass', function () {
 
 gulp.task('watch', function () {
     gulp.watch('scss/**/*.scss', ['sass']);
+});
+
+gulp.task('minify-js', function () {
+    
+    return gulp.src('js/main.js')
+        .pipe(uglify())
+        .pipe(rename('main.min.js'))
+        .pipe(gulp.dest('js/'))
+});
+
+gulp.task('minify-css', function () {
+
+    return gulp.src('css/styles.css')
+        .pipe(cssnano())
+        .pipe(rename('styles.min.css'))
+        .pipe(gulp.dest('css/'))
 });
